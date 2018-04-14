871e97b034581bee3e6403b9a1d89534b1418451
Samantha McVey <samantham@posteo.net>
2018-04-14 03:00:24 -0700

    Use LBL_ERR instead of ERR_ to match with libtommath
    The patch I got merged into libtommath used LBL_ERR instead, so use that
    so we don't get merge conflicts in the future.

--- bn_mp_reduce_2k.c
+++ bn_mp_reduce_2k.c
@@ -29,29 +29,29 @@ int mp_reduce_2k(mp_int *a, mp_int *n, mp_digit d)
 top:
    /* q = a/2**p, a = a mod 2**p */
    if ((res = mp_div_2d(a, p, &q, a)) != MP_OKAY) {
-      goto ERR_;
+      goto LBL_ERR;
    }
 
    if (d != 1) {
       /* q = q * d */
       if ((res = mp_mul_d(&q, d, &q)) != MP_OKAY) {
-         goto ERR_;
+         goto LBL_ERR;
       }
    }
 
    /* a = a + q */
    if ((res = s_mp_add(a, &q, a)) != MP_OKAY) {
-      goto ERR_;
+      goto LBL_ERR;
    }
 
    if (mp_cmp_mag(a, n) != MP_LT) {
       if ((res = s_mp_sub(a, n, a)) != MP_OKAY) {
-         goto ERR_;
+         goto LBL_ERR;
       }
       goto top;
    }
 
-ERR_:
+LBL_ERR:
    mp_clear(&q);
    return res;
 }
